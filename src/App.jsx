e2eb68ac9b5d55a7444476930a1af14d1b337f1e
Huuquy-HAu <quyhuu19@gmail.com>
2022-10-01 11:58:12 +0700

    Quy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import './App.css';
-import { Button, Modal, Form, message,Input, DatePicker,Space  } from 'antd';
+import { Button, Modal, Form, message, Input, DatePicker, Space } from 'antd';
 import 'antd/dist/antd.css';
 import { useForm } from 'rc-field-form';
 
@@ -10,7 +10,7 @@ import { useForm } from 'rc-field-form';
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [indexEdit,setIndexEdit] = useState(0)
+  const [indexEdit, setIndexEdit] = useState(0)
   const [ar, setAr] = useState([
     { name: 'Làm bài tập', description: "Làm luôn trong ngày hôm nay", deadline: '2022-10-03' }
   ])
@@ -36,7 +36,7 @@ function App() {
 
   const onFinish = (values) => {
     console.log(values);
-    console.log(39,indexEdit);
+    console.log(39, indexEdit);
     ar[indexEdit] = values
     setAr([...ar])
     message.success('Sửa thành công')
@@ -59,7 +59,7 @@ function App() {
       message.success('Thêm thành công')
       document.querySelector('.input-name').value = ''
       document.querySelector('.input-des').value = ''
-      document.querySelector('#input-date').value =''
+      document.querySelector('#input-date').value = ''
     }
   }
 
@@ -76,7 +76,8 @@ function App() {
           <button onClick={add}>Add</button>
         </div>
         <div className="todoList-output">
-          {ar.map((value, index) => {
+          {ar ? 
+          ar.map((value, index) => {
 
 
             const deleteBtn = () => {
@@ -85,11 +86,11 @@ function App() {
               message.success('Xóa thành công')
             }
             const showModal = () => {
-              console.log(90,index);
+              console.log(90, index);
               setIndexEdit(index)
               setIsModalOpen(true);
             };
-           
+
 
             return (
               <div className="data" key={index}>
@@ -108,12 +109,13 @@ function App() {
                 </div>
               </div>
             )
-          })
+          }):
+          ''
           }
         </div>
         <Modal title="Edit Form" footer={null} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-          <Form  name="basic" labelCol={{ span: 24, }} 
-            initialValues={{ remember: true  }}
+          <Form name="basic" labelCol={{ span: 24, }}
+            initialValues={{ remember: true }}
             onFinish={onFinish}
             onFinishFailed={onFinishFailed}
             autoComplete="off"
@@ -127,7 +129,7 @@ function App() {
                 },
               ]}
             >
-              <input type={Text} id='basic-name' placeholder='Name' style={{width:"100%", padding:'8px 16px', outline:'0',border:'none', borderBottom:"1px solid gray"}}/>
+              <input type={Text} id='basic-name' placeholder='Name' style={{ width: "100%", padding: '8px 16px', outline: '0', border: 'none', borderBottom: "1px solid gray" }} />
             </Form.Item>
 
             <Form.Item
@@ -140,7 +142,7 @@ function App() {
                 },
               ]}
             >
-              <input type={Text} id='basic_description' placeholder='description' style={{width:"100%", padding:'8px 16px', outline:'0',border:'none', borderBottom:"1px solid gray"}}/>
+              <input type={Text} id='basic_description' placeholder='description' style={{ width: "100%", padding: '8px 16px', outline: '0', border: 'none', borderBottom: "1px solid gray" }} />
             </Form.Item>
 
             <Form.Item
